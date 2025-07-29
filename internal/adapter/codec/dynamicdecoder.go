b382c74a323362f021081e7bb486e7259c2999c6
Alexandre Mahdhaoui <alexandre.mahdhaoui@gmail.com>
2025-07-29 22:17:36 +0200

    🌱 Refactor dynamic decoder
    Signed-off-by: Alexandre Mahdhaoui <alexandre.mahdhaoui@gmail.com>

--- internal/adapter/codec/dynamicdecoder.go
+++ internal/adapter/codec/dynamicdecoder.go
@@ -29,35 +29,17 @@ import (
 	yaml "sigs.k8s.io/yaml/goyaml.v3"
 )
 
-// TODO: implement the following hack:
-// 1. Use standard decoder to read yaml into map[any]any.
-// 2. Marshal that map into json bytes.
-// 3. UnmarshalJSON into target struct.
-
-// TODO: decoder can be simplified by directly decoding into map[any]any
-// 1. Check if m["items"] exists?
-// 2. YES -> walk (each item):
-//           - Get apiVersion + Kind
-//           - Get resource from apiServer
-//           - Dump resource to json bytes
-//           - Unmarshal json bytes into typed spec from apiServer
-// 3. NO  -> Same as above get apiVersion + kind && unmarshal the spec
-
-// TODO: write unit tests to works as intended
+// TODO: write unit tests
 
 // TODO: Not urgent:
 // - LTS would be adding Encoder/Decoder iface to "sigs.k8s.io/yaml"
-
-type drd struct {
-	apiServer types.APIServer
-}
 
 // Instantiate a new dynamic resource decoder. A dynamic resource decoder is a special codec
 // that can unmarshal one or many documents from any supported encoding.
 func NewDynamicResourceDecoder(
 	apiServer types.APIServer,
 ) types.DynamicDecoder[types.APIVersionKind] {
-	return drd{
+	return &rawDrd{
 		apiServer: apiServer,
 	}
 }
@@ -66,26 +48,14 @@ type rawDrd struct {
 	apiServer types.APIServer
 }
 
-func (d *rawDrd) decodeOne(v map[any]any) (types.Resource[types.APIVersionKind], error) {
-	// Ignore types assertion as "apiServer.Get" will return ERRNOTFOUND in the worst case scenario
-	apiVersion, _ := v["apiVersion"].(string)
-	kind, _ := v["kind"].(string)
-	avk := types.NewAPIVersionKind(apiVersion, kind)
-	out, err := d.apiServer.Get(avk)
-	if err != nil {
-		return types.Resource[types.APIVersionKind]{}, err
-	}
+var (
+	errInputMustBeJsonOrYaml = errors.New("input must be json or yaml")
+	errDecodingInput         = errors.New("error decoding input")
+	errInputMustNotBeEmpty   = errors.New("input must not be empty")
+)
 
-	b, err := json.Marshal(v)
-	if err != nil {
-		return types.Resource[types.APIVersionKind]{}, err
-	}
-
-	if err = json.Unmarshal(b, &out); err != nil {
-		return types.Resource[types.APIVersionKind]{}, err
-	}
-
-	return out, nil
+type resourceList[T any] struct {
+	Items []types.Resource[T] `json:"items" yaml:"items"`
 }
 
 // Decode implements types.DynamicDecoder.
@@ -141,9 +111,9 @@ func (d *rawDrd) Decode(reader io.Reader) ([]types.Resource[types.APIVersionKind
 		for i, obj := range objectList {
 			if v, ok := obj["items"]; ok {
 				// TODO: handle list items
-				list, ok := v.([]map[any]any)
+				list, ok := v.([]any)
 				if !ok {
-					return nil, errors.New("TODO")
+					return nil, flaterrors.Join(errors.New("TODO"), fmtErrAtIndex(i))
 				}
 				for _, raw := range list {
 					item, err := d.decodeOne(raw)
@@ -169,140 +139,39 @@ func (d *rawDrd) Decode(reader io.Reader) ([]types.Resource[types.APIVersionKind
 	return nil, flaterrors.Join(errInputMustBeJsonOrYaml, errDecodingInput)
 }
 
-// Instantiate a new dynamic resource decoder. A dynamic resource decoder is a special codec
-// that can unmarshal one or many documents from any supported encoding.
+func (d *rawDrd) decodeOne(v any) (types.Resource[types.APIVersionKind], error) {
+	m, ok := v.(map[string]any)
+	if !ok {
+		return types.Resource[types.APIVersionKind]{}, errors.New("TODO: decodeOne err")
+	}
+
+	// Ignore types assertion as "apiServer.Get" will return ERRNOTFOUND in the worst case scenario
+	apiVersion, _ := m["apiVersion"].(string)
+	kind, _ := m["kind"].(string)
+	avk := types.NewAPIVersionKind(apiVersion, kind)
+	out, err := d.apiServer.Get(avk)
+	if err != nil {
+		return types.Resource[types.APIVersionKind]{}, err
+	}
+
+	b, err := json.Marshal(m)
+	if err != nil {
+		return types.Resource[types.APIVersionKind]{}, err
+	}
+
+	if err = json.Unmarshal(b, &out); err != nil {
+		return types.Resource[types.APIVersionKind]{}, err
+	}
+
+	return out, nil
+}
+
 func NewRawDynamicResourceDecoder(
 	apiServer types.APIServer,
 ) types.DynamicDecoder[types.APIVersionKind] {
 	return &rawDrd{
 		apiServer: apiServer,
 	}
-}
-
-var (
-	errInputMustBeJsonOrYaml = errors.New("input must be json or yaml")
-	errDecodingInput         = errors.New("error decoding input")
-	errInputMustNotBeEmpty   = errors.New("input must not be empty")
-)
-
-type resourceList[T any] struct {
-	Items []types.Resource[T] `json:"items" yaml:"items"`
-}
-
-// Decode will try to decode the input as json and if it fails as yaml.
-// There are no detection mechanisms, the algorithm is really not optimized.
-//
-// The decoding operation is implemented as such:
-// 1. For enc := range [ json yaml ]
-// 2. For doc := range reader
-// 3. For type := range [ resourceList[Resource[T]] Resource[T] ]
-// 4. Decode type until input is exhausted
-func (d drd) Decode(reader io.Reader) ([]types.Resource[types.APIVersionKind], error) {
-	// not really clean but we want to copy the buffer
-	b, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(b) == 0 {
-		return nil, flaterrors.Join(errInputMustNotBeEmpty, errDecodingInput)
-	}
-
-	bCopy := make([]byte, len(b))
-	copied := copy(bCopy, b)
-	if copied < len(b) {
-		return nil, flaterrors.Join(errors.New("internal error"), errDecodingInput)
-	}
-
-	// -- [ json yaml ]
-	jsonBuf := bytes.NewBuffer(b)
-	yamlBuf := bytes.NewBuffer(bCopy)
-
-	for _, supportedDecoder := range []struct {
-		Decoder       decoder
-		UnmarshalFunc func(b []byte, v any) error
-	}{
-		{
-			Decoder:       json.NewDecoder(jsonBuf),
-			UnmarshalFunc: json.Unmarshal,
-		}, {
-			Decoder:       yaml.NewDecoder(yamlBuf),
-			UnmarshalFunc: yaml.Unmarshal,
-		},
-	} {
-		rawRes := make([]types.Resource[any], 0)
-		// -- resourceList[T]
-		rls, rlsErr := decode[resourceList[any]](supportedDecoder.Decoder)
-		for _, rl := range rls {
-			rawRes = append(rawRes, rl.Items...)
-		}
-
-		// -- types.Resource[T]
-		rs, rsErr := decode[types.Resource[any]](supportedDecoder.Decoder)
-		rawRes = append(rawRes, rs...)
-
-		// -- input is json/yaml but received error while parsing
-		if len(rawRes) > 0 && rlsErr != nil && rsErr != nil {
-			return nil, flaterrors.Join(
-				errDecodingInput,
-				rsErr,
-				rlsErr,
-			)
-		}
-
-		// -- input not decoded: try another supported decoder
-		if len(rawRes) == 0 {
-			continue
-		}
-
-		b, _ = yaml.Marshal(rawRes)
-		fmt.Println(string(b))
-
-		// -- unmarshal inner objects
-		out := make([]types.Resource[types.APIVersionKind], 0)
-		for i, r := range rawRes {
-			v, err := d.apiServer.Get(types.NewAVKFromResource(r))
-			if err != nil {
-				return nil, flaterrors.Join(err, fmtErrAtIndex(i))
-			}
-
-			v.Metadata = r.Metadata
-
-			b, err := json.Marshal(r.Spec)
-			if err != nil {
-				return nil, flaterrors.Join(err, fmtErrAtIndex(i))
-			}
-
-			if err := supportedDecoder.UnmarshalFunc(b, &v.Spec); err != nil {
-				return nil, flaterrors.Join(err, fmtErrAtIndex(i))
-			}
-
-			out = append(out, v)
-		}
-
-		// -- At this point, the output can be safely returned
-		return out, nil
-	}
-
-	// -- input must be invalid
-	return nil, flaterrors.Join(errInputMustBeJsonOrYaml, errDecodingInput)
-}
-
-func decode[T any](d decoder) ([]T, error) {
-	out := make([]T, 0)
-	done := false
-	i := 0
-	for !done {
-		v := new(T)
-		if err := d.Decode(v); errors.Is(err, io.EOF) {
-			done = true // End of file/stream
-		} else if err != nil {
-			return nil, flaterrors.Join(err, fmtErrAtIndex(i))
-		}
-		i++
-		out = append(out, *v)
-	}
-	return out, nil
 }
 
 // raw decoding into map[any]any
